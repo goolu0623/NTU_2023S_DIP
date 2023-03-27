ea6383ea80c4b20f304f14298c3dc830b4a6cbd8
goolu0623 <weng840623@gmail.com>
2023-03-27 15:06:12 +0800

    HW2/p1_finished

--- HW2/functions.py
+++ HW2/functions.py
@@ -1,43 +1,153 @@
 import cv2
 import numpy as np
 import math
+import matplotlib.pyplot as plt
 
 
-# def Sobel(image, mask_size, k_value):
-#     row, col = image.shape
-#     for row_num in range(row):
-#         for col_num in range(col):
-#     pass
+def gradient_image(image, type):
+
+    if type == 'SOBEL':
+        K = 2
+    elif type == 'PREWITT':
+        K = 1
+    col_mask = np.array([[1, K, 1], [0, 0, 0], [-1, -K, -1]])
+    row_mask = np.rot90(col_mask)
+    K_size = len(col_mask)
+    pad = K_size//2
+    grad_image = image.copy()
+    pad_image = np.lib.pad(image,(pad,pad),'reflect')
+
+    for i in range(len(grad_image)):
+        for j in range(len(grad_image[i])):
+            row_sum, col_sum = 0, 0
+            for r in range(len(row_mask)):
+                for c in range(len(row_mask[r])):
+                    row_sum += pad_image[i + r ][j + c ] * row_mask[r][c]
+                    col_sum += pad_image[i + r ][j + c ] * col_mask[r][c]
+            gradientMagnitude = math.sqrt(row_sum ** 2 + col_sum ** 2)
+            grad_image[i][j] = gradientMagnitude
+    return grad_image
 
 
-# def Prewitt(image, threshold, p1mask, p2mask):
-#     PrewittImage = image.copy()
-#     imageBoarded = Padding('DUPLICATE', 1, 1, 1, 1, PrewittImage)
-#     for i in range(len(image)):
-#         for j in range(len(image[i])):
-#             p1sum, p2sum = 0, 0
-#             for r in range(len(p1mask)):
-#                 for c in range(len(p1mask[r])):
-#                     p1sum += imageBoarded[i + r - 1][j + c - 1] * p1mask[r][c]
-#                     p2sum += imageBoarded[i + r - 1][j + c - 1] * p2mask[r][c]
-#             gradientMagnitude = math.sqrt(p1sum ** 2 + p2sum ** 2)
-#             PrewittImage[i][j] = 0 if gradientMagnitude >= threshold else 255
-#     return PrewittImage
-
-def GaussianFilter(img,K_size, sigma):
-    h, w = img.shape
+def threshld_image(image, threshold):
+    result_image = image.copy()
+    result_image[image > threshold] = 255
+    result_image[image <= threshold] = 0
+    return result_image
 
 
+def orientation_image(image, type):
+    if type == 'SOBEL':
+        K = 2
+    elif type == 'PREWITT':
+        K = 1
+    col_mask = np.array([[-1, -K, -1], [0, 0, 0], [1, K, 1]])
+    row_mask = np.rot90(col_mask)
+    K_size = len(col_mask)
+    pad = K_size//2
+
+
+    theta_image = image.copy().astype(float)
+    pad_image = np.lib.pad(image,(pad,pad),'reflect')
+    for i in range(len(image)):
+        for j in range(len(image[i])):
+            row_sum, col_sum = 0, 0
+            for r in range(len(row_mask)):
+                for c in range(len(row_mask[r])):
+                    row_sum += pad_image[i + r - 1][j + c - 1] * row_mask[r][c]
+                    col_sum += pad_image[i + r - 1][j + c - 1] * col_mask[r][c]
+            theta_image[i][j] = math.atan(col_sum / row_sum) * 180.0 / math.pi
+    return theta_image
+
+
+def non_maximal_suppression(grad_image, ori_image):
+    row = grad_image.shape[0]
+    col = grad_image.shape[1]
+    non_maximal = grad_image.copy()
+    ori_image[ori_image < 0] += 180
+    for i in range(1, row - 1):
+        for j in range(1, col - 1):
+            G1 = 0
+            G2 = 0
+            # angle == 0
+            if ((0 <= ori_image[i][j] < 22.5) or (157.5 < ori_image[i][j] <= 180)):
+                G1 = grad_image[i][j + 1]
+                G2 = grad_image[i][j - 1]
+            # angle == 45
+            elif (22.5 <= ori_image[i][j] <= 67.5):
+                G1 = grad_image[i + 1][j - 1]
+                G2 = grad_image[i - 1][j + 1]
+            # angle == 90
+            elif (67.5 < ori_image[i][j] < 112.5):
+                G1 = grad_image[i + 1][j]
+                G2 = grad_image[i - 1][j]
+            # angle == 135
+            elif (112.5 <= ori_image[i][j] <= 157.5):
+                G1 = grad_image[i - 1][j - 1]
+                G2 = grad_image[i + 1][j + 1]
+
+            if (grad_image[i][j] >= G1) and (grad_image[i][j] >= G2):
+                non_maximal[i][j] = grad_image[i][j]
+            else:
+                non_maximal[i][j] = 0
+    return non_maximal
+
+
+def hysteretic_thresholding(image, threshold_H, threshold_L):
+    row, col = image.shape
+    threshold_image = np.zeros((row, col), dtype=np.uint8)
+    for i in range(row):
+        for j in range(col):
+            if (image[i][j] >= threshold_H):
+                threshold_image[i][j] = 2
+            elif (image[i][j] >= threshold_L):
+                threshold_image[i][j] = 1
+            else:
+                threshold_image[i][j] = 0
+    return threshold_image
+
+
+def set_edge_point(image, i, j):  # recursively
+    row, col = image.shape
+    if i < 0 or j < 0 or i >= row or j >= col:
+        return 0
+
+    for k in range(3):
+        for l in range(3):
+            if (i + k - 1 >= 0 and i + k - 1 < row and j + l - 1 >= 0 and j + l - 1 < col):
+                if (image[i + k - 1][j + l - 1] == 1):
+                    image[i][j] = set_edge_point(image, i + k - 1, j + l - 1)
+                if (image[i + k - 1][j + l - 1] == 2):
+                    return 255
+                else:
+                    return 0
+
+
+def connected_component_labeling(image):
+    row, col = image.shape
+    for i in range(row):
+        for j in range(col):
+            if (image[i][j] == 2):  # edge
+                image[i][j] = 255
+            elif (image[i][j] == 1):  # candidate
+                set_edge_point(image, i, j)
+    return image
+
+
+def GaussianFilter(grad_image, K_size, sigma):
+    # reference https://blog.csdn.net/qq_28368377/article/details/107288647
+    # 從上面修改成我的padding方式+變成灰階版本
+    h, w = grad_image.shape
 
     # duplicate padding
     pad = K_size // 2
-    out = Padding('DUPLICATE', pad,pad,pad,pad, img).astype(np.float)
+    out = np.lib.pad(grad_image,(pad,pad),'reflect').astype(float)
 
     # out = np.zeros((h + 2 * pad, w + 2 * pad), dtype=np.float)
-    # out[pad:pad + h, pad:pad + w] = img.copy().astype(np.float)
+    # out[pad:pad + h, pad:pad + w] = grad_image.copy().astype(np.float)
 
-    # 定义滤波核
-    K = np.zeros((K_size, K_size), dtype=np.float)
+    # kernel mask
+    K = np.zeros((K_size, K_size), dtype=float)
 
     for x in range(-pad, -pad + K_size):
         for y in range(-pad, -pad + K_size):
@@ -56,52 +166,101 @@ def GaussianFilter(img,K_size, sigma):
     return out
 
 
-def Padding(padding_type, left, right, top, bottom, image):
-    # 先只能吃灰階
 
-    # 預計實作: zero, duplicate, mirror
-    row, col = image.shape[0], image.shape[1]
-    Result_image = np.zeros((row + top + bottom, col + left + right), np.uint8)
-    # ZERO padding:
-    if padding_type == 'ZERO':
-        for row_num in range(row):
-            for col_num in range(col):
-                Result_image[row_num + top][col_num + left] = image[row_num][col_num]
 
-    # DUPLICATE padding:
-    if padding_type == 'DUPLICATE':
-        for row_num in range(row):
-            for col_num in range(col):
-                Result_image[row_num + top][col_num + left] = image[row_num][col_num]
-        # 左
-        for row_num in range(row):
-            for col_num in range(left):
-                Result_image[row_num + top][col_num] = image[row_num][0]
-        # 右
-        for row_num in range(row):
-            for col_num in range(right):
-                Result_image[row_num + top][col_num + col + left] = image[row_num][col - 1]
-        # 上
-        for row_num in range(top):
-            for col_num in range(col):
-                Result_image[row_num][col_num + left] = image[0][col_num]
-        # 下
-        for row_num in range(bottom):
-            for col_num in range(col):
-                Result_image[row + row_num + top][col_num + left] = image[row - 1][col_num]
+def Derivative_gradient(image):
 
-    # Mirror padding:
-    # if padding_type == 'MIRROR':
-    #     for row_num in range(row):
-    #         for col_num in range(col):
-    #             Result_image[row_num+top][col_num+left] = image[row_num][col_num]
-    #
-    #     # top
-    #     for row_num in range(top):
-    #         for col_num in range(col):
-    #             Result_image[-row_num+top][col_num+left] = image[-row_num+top]
-    #     # bottom
-    #     # left
-    #     # right
-    #     # 四個角落
-    return Result_image
+    lp_kernel = np.array(
+        [[-2, 1, -2],
+         [1, 4, 1],
+         [-2, 1, -2]]
+    )/8
+    K_size = len(lp_kernel)
+    pad = K_size//2
+
+    pad_image = np.lib.pad(image,(pad,pad),'reflect')
+    lp_image = np.zeros((image.shape), dtype=float)
+    for i in range(len(image)):
+        for j in range(len(image[i])):
+            grad_sum = 0.0
+            for r in range(len(lp_kernel)):
+                for c in range(len(lp_kernel[r])):
+                    grad_sum += pad_image[i + r - 1][j + c - 1] * lp_kernel[r][c]
+            lp_image[i][j] = grad_sum
+    return lp_image
+
+
+def edge_crispening(org_image, gauss_image, c):
+    edge_crispening_image = org_image.copy().astype(np.uint8)
+    for i in range(len(org_image)):
+        for j in range(len(org_image[i])):
+            value = (c / (2.0 * c - 1)) * org_image[i][j] - ((1 - c) / (2.0 * c - 1)) * gauss_image[i][j]
+            if value < 0:
+                value = 0
+            if value > 255:
+                value = 255
+            edge_crispening_image[i][j] = value
+    return edge_crispening_image
+
+
+def hough_transform(image, n):
+
+    # Sobel edge detect
+    grad_image = gradient_image(image, 'SOBEL')
+    edge_image = threshld_image(grad_image, 150)
+
+    # Generate sin and cos look-up table
+    sin_tab = np.zeros(180)
+    cos_tab = np.zeros(180)
+
+    for angle in range(180):
+        theta = angle*3.14159265358979/180.0
+        cos_tab[angle] = np.cos(theta)
+        sin_tab[angle] = np.sin(theta)
+
+    # compute hough_img
+    feature_point = 255
+    row, col = edge_image.shape
+    rmax = int(math.hypot(row, col))
+    hough_image = np.zeros((180, rmax*2))
+
+    for i in range(row):
+        for j in range(col):
+            if edge_image[i][j] == feature_point:
+                for angle in range(180):                # for each angle
+                    p = i*sin_tab[angle]+j*cos_tab[angle]  # compute p
+                    p = int(p)                            # shift r to positive value
+                    hough_image[angle][p+rmax] += 1       # accumulation
+    plt.imshow(hough_image, cmap='gray')
+
+    # draw top n line
+    a = hough_image.reshape(1, -1)[0]
+    index = np.argpartition(a, -n)[-n:]
+    for idx in index:
+        angle, p = np.unravel_index(idx, hough_image.shape)
+        p -= rmax
+
+        a = sin_tab[angle]
+        b = cos_tab[angle]
+
+        # 注意除以0的case
+        if b < 1e-12:
+            x0 = col
+            y0 = (p-x0*b)/a
+
+            x1 = 0
+            y1 = (p-x1*b)/a
+        else:
+            y0 = row
+            x0 = (p-y0*a)/b
+
+            y1 = 0
+            x1 = (p-y1*a)/b
+
+        x0 = int(x0)
+        y0 = int(y0)
+        x1 = int(x1)
+        y1 = int(y1)
+
+        cv2.line(image, (x0, y0), (x1, y1), (255), 2)
+    return image
--- HW2/hw2.py
+++ HW2/hw2.py
@@ -4,38 +4,13 @@ import matplotlib.pyplot as plt
 from functions import *
 
 
-def problem_1_a():
-    sample1 = cv2.imread("hw2_sample_images/sample1.png", cv2.IMREAD_GRAYSCALE)
-    row_mask = np.array([[-1, -2, -1], [0, 0, 0], [1, 2, 1]])
-    col_mask = np.rot90(row_mask)
-    threshold = 38
+def problem_1_a(image):
 
-    result1 = sample1.copy()
-    result2 = sample1.copy()
-    theta_image = sample1.copy().astype(np.float)
-    imageBoarded = Padding('DUPLICATE', 1, 1, 1, 1, result1)
-    for i in range(len(result1)):
-        for j in range(len(result1[i])):
-            row_sum, col_sum = 0, 0
-            for r in range(len(row_mask)):
-                for c in range(len(row_mask[r])):
-                    row_sum += imageBoarded[i + r - 1][j + c - 1] * row_mask[r][c]
-                    col_sum += imageBoarded[i + r - 1][j + c - 1] * col_mask[r][c]
-            theta_pixel = math.atan(col_sum / row_sum)
-            gradientMagnitude = math.sqrt(row_sum ** 2 + col_sum ** 2)
-            result1[i][j] = gradientMagnitude
-            result2[i][j] = 0 if gradientMagnitude >= threshold else 255
-            theta_image[i][j] =  theta_pixel
-
-    cv2.imshow('1', result1)
-    cv2.imshow('2', result2)
-    cv2.waitKey()
-    cv2.destroyAllwindows()
-    return
+    cv2.imwrite("result1.png", gradient_image(image, 'SOBEL'))
+    cv2.imwrite("result2.png", threshld_image(gradient_image(image, 'SOBEL'), 100))
 
 
-def problem_1_b():
-    sample1 = cv2.imread("hw2_sample_images/sample1.png", cv2.IMREAD_GRAYSCALE)
+def problem_1_b(image):
     '''
     canny edge detection: 
     five steps
@@ -45,21 +20,96 @@ def problem_1_b():
     4. Hysteretic thresholding
     5. Connected component labeling method
     '''
-    gaus_image = GaussianFilter(sample1, K_size=5, sigma=1.3)
-    grad_image =
-    pass
+    # step1: Noise Reduction
+    gauss_image = GaussianFilter(image, K_size=3, sigma=0.01)
+
+    # step2: Compute Gradient Magnitude and Orientation
+    grad_image = gradient_image(image=gauss_image, type='SOBEL')
+    ori_image = orientation_image(image=gauss_image, type='SOBEL')
+
+    # step3: Non-maximal suppression
+    non_max_image = non_maximal_suppression(grad_image, ori_image)
+
+    # step4: Hysteretic thresholding
+    threshold_image = hysteretic_thresholding(non_max_image, 100, 5)
+
+    # step5: Connected Component Labeling Method
+    canny_image = connected_component_labeling(threshold_image)
+
+    cv2.imwrite('result3.png', canny_image)
 
 
-def problem_1_c():
-    pass
+def problem_1_c(image):
+
+    # step1: Gaussian Filter
+    gauss_image = GaussianFilter(image, 3, 0.01)
+
+    # step2: 2nd-order Derivative(Laplacian)
+    lp_image = Derivative_gradient(gauss_image)
+
+    hist, bins = np.histogram(lp_image, bins=1000, range=(lp_image.min(), lp_image.max()))
+    bins = bins[:-1]
+    plt.bar(bins, hist)
+    plt.title('hist')
+    plt.savefig('hist.png')
+
+    # step3: Thresholded
+    row, col = lp_image.shape
+    T = 1.2
+    threshold_image = lp_image.copy()
+    for i in range(len(threshold_image)):
+        for j in range(len(threshold_image[i])):
+            threshold_image[i][j] = 0 if lp_image[i][j] > T else 255
+
+    # step4: Zero-crossing
+    zero_image = np.zeros((row, col))
+    for i in range(1, row - 1):
+        for j in range(1, col - 1):
+            if threshold_image[i][j] == 0:
+                if (lp_image[i-1][j-1]*lp_image[i+1][j+1] < 0 or lp_image[i-1][j]*lp_image[i+1][j] < 0 or
+                        lp_image[i-1][j+1]*lp_image[i+1][j-1] < 0 or lp_image[i][j-1]*lp_image[i][j+1] < 0):
+                    zero_image[i][j] = 255
+
+    cv2.imwrite('result4.png', zero_image)
 
 
-def problem_1_d():
-    pass
+def problem_1_d(image):
+    # c值介於0.6~0.83之間
+    image = image
+    gauss_image = GaussianFilter(image, 10, 2)
+    edge_crispening_image = edge_crispening(image, gauss_image, c=0.6)
+    cv2.imwrite('result5.png', edge_crispening_image)
 
 
-def problem_1_e():
-    pass
+def problem_1_e(image):
+    '''
+    canny edge detection: 
+    five steps
+    1. Noise reduction
+    2. Compute gradient magnitude and orientation
+    3. Non-maximal suppression
+    4. Hysteretic thresholding
+    5. Connected component labeling method
+    '''
+    # step1: Noise Reduction
+    gauss_image = GaussianFilter(image, K_size=3, sigma=0.1)
+
+    # step2: Compute Gradient Magnitude and Orientation
+    grad_image = gradient_image(image=gauss_image, type='SOBEL')
+    ori_image = orientation_image(image=gauss_image, type='SOBEL')
+
+    # step3: Non-maximal suppression
+    non_max_image = non_maximal_suppression(grad_image, ori_image)
+
+    # step4: Hysteretic thresholding
+    threshold_image = hysteretic_thresholding(non_max_image, 150, 50)
+
+    # step5: Connected Component Labeling Method
+    canny_image = connected_component_labeling(threshold_image)
+
+    cv2.imwrite('result6.png', canny_image)
+
+    cv2.imwrite('result7.png', hough_transform(canny_image, 100))
 
 
 def problem_2_a():
@@ -71,13 +121,36 @@ def problem_2_b():
 
 
 def test():
-    sample1 = cv2.imread("hw2_sample_images/sample1.png", cv2.IMREAD_GRAYSCALE)
-    cv2.imshow('test', GaussianFilter(sample1))
-    cv2.imshow('samp', sample1)
-    cv2.waitKey()
-    cv2.destroyAllwindows()
+
+    pass
 
 
 if __name__ == '__main__':
-    test()
-    # problem_1_a()
+
+    sample1 = cv2.imread("./hw2_sample_images/sample1.png", cv2.IMREAD_GRAYSCALE)
+    sample2 = cv2.imread("./hw2_sample_images/sample2.png", cv2.IMREAD_GRAYSCALE)
+    sample3 = cv2.imread("./hw2_sample_images/sample3.png", cv2.IMREAD_GRAYSCALE)
+    sample5 = cv2.imread("./hw2_sample_images/sample5.png", cv2.IMREAD_GRAYSCALE)
+    # # test()
+    # problem_1_a(sample1)
+    # problem_1_b(sample1)
+    # problem_1_c(sample1)
+    # problem_1_d(sample2)
+    # result5 = cv2.imread("./result5.png", cv2.IMREAD_GRAYSCALE)
+    # problem_1_e(result5)
+    # result6 = cv2.imread("./result6.png", cv2.IMREAD_GRAYSCALE)
+    # cv2.imwrite('hough1.png', hough_transform(result6, 1))
+    cvguass = cv2.GaussianBlur(sample2, (3,3), 0)
+    cvcanny = cv2.Canny(cvguass, 50, 150)
+    lines = cv2.HoughLinesP(cvcanny, 1, np.pi/180, 1, np.array([]),10,1)
+    line_image = np.copy(sample2)*0
+    for line in lines:
+        for x1,y1,x2,y2 in line:
+            cv2.line(line_image,(x1,y1),(x2,y2),(255,0,0),10)
+    combo = cv2.addWeighted(cvcanny, 0.8, line_image, 0.2, 1)
+    cv2.imshow('hough', combo)
+    cv2.imshow('canny', cvcanny)
+    # cv2.imshow('result6',result6)
+    # cv2.imshow('cv_hough', cvhough)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
